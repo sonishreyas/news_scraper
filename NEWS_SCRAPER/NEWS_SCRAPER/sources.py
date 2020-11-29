e6c47b811fc73bf5db06840b3c3ee6950529a782
sonishreyas <shreyas@rapidken.ai>
2020-11-30 01:29:19 +0530

    initial scraping code

---
+++ NEWS_SCRAPER/NEWS_SCRAPER/sources.py
@@ -0,0 +1,13 @@
+news_websites = [
+    {
+        "url" : "https://timesofindia.indiatimes.com/news",
+        "topic" : "Latest News",
+        "source" : "Times of India",
+        "articles_xpath" : "//*[@id='content']/div/div[2]",
+        "title_xpath" : "//*[@id='app']/div/div[4]/div[1]/div[2]/div[1]/h1",
+        "description_xpath" : "//*[@id='app']/div/div[4]/div[1]/div[2]/div[2]/div[2]/div",
+        "image_xpath" : "//*[@id='app']/div/div[4]/div[1]/div[2]/div[2]/section/div/div/img",
+        "author_xpath" : "//*[@id='app']/div/div[4]/div[1]/div[2]/div[1]/div/div[1]"
+    }
+    
+]
--- NEWS_SCRAPER/NEWS_SCRAPER/spiders/spider.py
+++ NEWS_SCRAPER/NEWS_SCRAPER/spiders/spider.py
@@ -1,4 +1,35 @@
 import scrapy
+from NEWS_SCRAPER.sources import *
+import pandas as pd
 
 class NewsSpider(scrapy.Spider):
-     
+    name = 'news'
+
+    def __init__(self, **kwargs):
+        self.url = kwargs.get("url")
+        self.topic = kwargs.get("topic")
+        self.articles_xpath = kwargs.get("articles_xpath")
+        self.title_xpath = kwargs.get("title_xpath")
+        self.description_xpath = kwargs.get("description_xpath")
+        self.image_xpath = kwargs.get("image_xpath")
+        self.author_xpath = kwargs.get("author_xpath")
+        
+    def start_requests(self):
+        yield scrapy.Request(url=self.url, callback=self.parse)
+    
+    def parse(self, response):
+        print("In parse method.")
+        self.save(response.request.url, response.text)
+
+    def save(self, source_url, xpath_response):
+        # mydb = dbConnection()
+        # mycursor = mydb.cursor()
+        # news_table = news_table       
+        # sql = "INSERT INTO " + news_table + " (url, title, description, author, image, topic, source) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        # source_url = urllib.parse.quote(source_url)
+        # mycursor.execute(sql, val)
+        # mydb.commit()
+        # mycursor.close()
+        print(source_url)
+
+        
